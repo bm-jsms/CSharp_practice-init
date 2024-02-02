6614499c171a9d9a9cf035ff999c85490a677850
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-02 14:55:14 -0500

    exercise - 2

--- Program.cs
+++ Program.cs
@@ -24,3 +24,24 @@ sideB = Convert.ToDouble(Console.ReadLine());
 
 result = sideA * sideB;
 Console.WriteLine("\nSide A: " + sideA + "\tSide B: " + sideB + "\nThe area of reactangle is: " + result);
+
+
+
+Console.WriteLine("\n" + new string('-', 50) + "\n");
+
+
+
+Console.WriteLine("Calculate the area of a Circle!!");
+
+var radio = 1.1;
+var resultCircle = 1.1;
+
+const double Pi = 3.14;
+
+// Pi * radio*radio
+Console.WriteLine("Enter radio value:");
+radio = Convert.ToDouble(Console.ReadLine());
+
+resultCircle = Pi * radio * radio;
+
+Console.WriteLine("the area of the circle is: " + resultCircle);
