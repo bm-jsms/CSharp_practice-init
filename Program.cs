63d83d395fdf34cd3bc195abcc9f3625d9193301
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-02 14:48:34 -0500

    exercise - 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!"); 
+
+
+
+
+Console.WriteLine("\n" + new string('-', 50) + "\n");
+
+
+
+// program that calculates the area of ​​a rectangle
+
+var sideA = 1.1;
+var sideB = 1.1;
+var result = 1.1;
+
+Console.WriteLine("Calculate the area of a rectangle!!");
+
+Console.WriteLine("Insert value A: ");
+sideA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Insert value B: ");
+sideB = Convert.ToDouble(Console.ReadLine());
+
+result = sideA * sideB;
+Console.WriteLine("\nSide A: " + sideA + "\tSide B: " + sideB + "\nThe area of reactangle is: " + result);
