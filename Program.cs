55de9528f59a58a0d8c1e93764b224cafad92e53
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-02 15:38:43 -0500

    logical operators

--- Program.cs
+++ Program.cs
@@ -45,3 +45,38 @@ radio = Convert.ToDouble(Console.ReadLine());
 resultCircle = Pi * radio * radio;
 
 Console.WriteLine("the area of the circle is: " + resultCircle);
+
+
+
+Console.WriteLine("\n" + new string('-', 50) + "\n");
+
+
+
+// logical operators
+/*
+&& = and
+|| = or
+! = not
+^ = xor => if both values are different, the result is true
+*/
+
+// bool value1 = true;
+// bool value2 = false;
+// bool value3 = true;
+
+var (value1, value2, value3) = (true, false, true);
+
+bool result1 = value1 && value2;
+Console.WriteLine("value1 && value2: " + result1); // false
+
+bool result2 = value1 || value2;
+Console.WriteLine("value1 || value2: " + result2); // true
+
+bool result3 = !value3;
+Console.WriteLine("!value1: " + result3); // false
+
+bool result4 = (value1 && value2) || value3;
+Console.WriteLine("(value1 && value2) || value3: " + result4); // true
+
+bool result5 = value1 ^ value2;
+Console.WriteLine("value1 ^ value2: " + result5); // true
